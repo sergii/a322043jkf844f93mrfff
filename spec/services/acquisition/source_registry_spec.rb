258b37bce27a9e463404f3d9c0e9228150735f05
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-02 14:21:57 +0300

    Implement validated LMX config loading

---
+++ spec/services/acquisition/source_registry_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Acquisition::SourceRegistry, type: :model do
+  it "exposes configured source identifiers and acquisition strategies" do
+    expect(described_class.source_ids).to eq([ "dou" ])
+    expect(described_class.enabled?("dou")).to be(true)
+    expect(described_class.acquisition_strategies("dou")).to eq(
+      [
+        { "type" => "http_html", "preference" => "primary", "status" => "evaluate" },
+        { "type" => "browser", "preference" => "fallback", "status" => "evaluate" }
+      ]
+    )
+    expect(described_class.primary_strategy("dou")).to eq(
+      "type" => "http_html",
+      "preference" => "primary",
+      "status" => "evaluate"
+    )
+  end
+
+  it "fails explicitly for an unknown source" do
+    expect { described_class.fetch("missing") }
+      .to raise_error(KeyError, /Unknown acquisition source/)
+  end
+end
